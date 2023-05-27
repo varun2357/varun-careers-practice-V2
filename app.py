fd35ea7deb43cfafb9ac16e20231562455d8b18a
varun2357 <56508525+varun2357@users.noreply.github.com>
2023-05-27 08:25:44 +0000

    added jobs and apply button + json

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': "Data analyst",
+    'location': "Hyderabad, India",
+    'salary': "Rs. 10,00,000"
+  },
+  {
+    'id': 2,
+    'title': "Data scientist",
+    'location': "Hyderabad, India",
+    'salary': "Rs. 15,00,000"
+  },
+  {
+    'id': 3,
+    'title': "Frontend Engineer",
+    'location': "Bengaluru, India",
+    'salary': "Rs. 9,00,000"
+  },
+  {
+    'id': 4,
+    'title': "Data Engineer",
+    'location': "New York, USA",
+    'salary': "$100,000"
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="Varun")
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
